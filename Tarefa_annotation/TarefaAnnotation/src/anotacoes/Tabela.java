65d4c22060321e5906f1d4a863ff845b5d9d2d85
DjalmaOlivei <djalma.costa.filho@gmail.com>
2026-01-11 22:04:44 -0300

    Tarefa de Anotations EBAC mod 17, Tabela annotation...!!!

---
+++ Tarefa_annotation/TarefaAnnotation/src/anotacoes/Tabela.java
@@ -0,0 +1,11 @@
+package anotacoes;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Tabela {
+
+    String nome();
+}
