8dae25b3c7c2efbf98df769e515cdf97ec0e8eb3
DjalmaOlivei <djalma.costa.filho@gmail.com>
2026-01-13 05:41:23 -0300

    Tarefa de Teste 2 EBAC mod 24, testado 2 ...!!!

---
+++ Tarefe_teste2/ExemplosMod23e24/src/br/com/rpires/dao/IContratoDao.java
@@ -0,0 +1,13 @@
+package br.com.rpires.dao;
+
+/**
+ * @author rodrigo.pires
+ */
+public interface IContratoDao {
+    void salvar();
+    String buscar();
+    void excluir();
+    void atualizar();
+    //TODO
+    //Fazer métodos de buscar, excluir e atualizar
+}
---
+++ Tarefe_teste2/ExemplosMod23e24/src/br/com/rpires/dao/mocks/ContratoDaoMock.java
@@ -0,0 +1,26 @@
+package br.com.rpires.dao.mocks;
+
+import br.com.rpires.dao.IContratoDao;
+
+/**
+ * @author rodrigo.pires
+ */
+public class ContratoDaoMock implements IContratoDao {
+
+    @Override
+    public void salvar() {
+
+    }
+
+    public String buscar() {
+    return "Sucesso";
+    }
+
+    public void excluir() {
+
+    }
+
+    public void atualizar() {
+
+    }
+}
