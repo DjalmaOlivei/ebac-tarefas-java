11b62b0d5ced73011a2e192fd6e8320ef3a50f50
DjalmaOlivei <djalma.costa.filho@gmail.com>
2026-06-06 13:42:41 -0300

    upload tarefas mod 39  upload local server com wildfly, e 40 boilerplate springboot start.spring.io

---
+++ tarefa_spring_boilerplate/SpringBootPrimeioExemplo.zip_expanded/SpringBootPrimeioExemplo/src/main/java/br/com/dj/SpringBootPrimeioExemplo/SpringBootPrimeioExemploApplication.java
@@ -0,0 +1,13 @@
+package br.com.dj.SpringBootPrimeioExemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrimeioExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrimeioExemploApplication.class, args);
+	}
+
+}
